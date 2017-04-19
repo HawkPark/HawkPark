d5b5c987b0161139dc72448964e67ddb2c6bbe62
priya <jkster@gmail.com>
2017-04-19 16:15:53 -0400

    add strings

--- app/src/main/java/com/example/android/hawkpark02/HomeActivity.java
+++ app/src/main/java/com/example/android/hawkpark02/HomeActivity.java
@@ -2,6 +2,9 @@ package com.example.android.hawkpark02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ScrollView layout = (ScrollView) findViewById(R.id.sv_lot_bttn_ha);
+
+
+
     }
 }
